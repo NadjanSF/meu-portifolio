4b2f9591ac94ca1b4f8266d7507998ad4b89d309
NadjanSF <nda-ph@hotmail.com>
2025-04-14 18:32:13 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+'use strict';
+const linkedinBtn = document.getElementById('linkedinBtn');
+
+linkedinBtn.addEventListener('click', function() {
+    window.open("https://www.linkedin.com/in/nadjan-santos-florencio-b87453138/", "_blank");
+});
