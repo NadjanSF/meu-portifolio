c020d8e52bea157c48c3338f8983c65af3a11822
NadjanSF <nda-ph@hotmail.com>
2025-06-16 17:08:19 -0300

    Delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-const linkedinBtn = document.getElementById('linkedinBtn');
-
-linkedinBtn.addEventListener('click', function() {
-    window.open("https://www.linkedin.com/in/nadjan-santos-florencio-b87453138/", "_blank");
-});
